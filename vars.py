3fad354a76ee381e7f6572fbbb571391c6d9edc5
botdlbot9-rgb <botdlbot9@gmail.com>
2026-05-04 12:00:53 +0530

    vars.py

--- vars.py
+++ vars.py
@@ -3,6 +3,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "35524250")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "98de9f02ef055387a1cf3eda6220a18e") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "8681279035:AAHUCzGrNFS2WgL5zGm4aJsm7XW3nerR9g0") #Replace with your bot token
